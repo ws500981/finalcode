32875a32909d37d605d8f1c2ee1ddb429aa45505
WS <47887674+ws500981@users.noreply.github.com>
2021-07-19 22:23:07 +0800

    dnabert

---
+++ DNABERT/examples/dnabert_finetune.sh
@@ -0,0 +1,26 @@
+export KMER=3
+export MODEL_PATH=3-new-12w-0
+export DATA_PATH=sample_data
+export OUTPUT_PATH=./filtered_0.9_bybase_3mer
+python -u run_finetune_withdebugmessages.py \
+    --model_type dna \
+    --tokenizer_name=dna$KMER \
+    --model_name_or_path $MODEL_PATH \
+    --task_name SP \
+    --do_train \
+    --do_eval \
+    --data_dir $DATA_PATH \
+    --max_seq_length 75 \
+    --per_gpu_eval_batch_size=16   \
+    --per_gpu_train_batch_size=16   \
+    --learning_rate 2e-4 \
+    --num_train_epochs 3.0 \
+    --output_dir $OUTPUT_PATH \
+    --evaluate_during_training \
+    --logging_steps 100 \
+    --save_steps 4000 \
+    --warmup_percent 0.1 \
+    --hidden_dropout_prob 0.1 \
+    --overwrite_output \
+    --weight_decay 0.01 \
+    --n_process 8 > filtered_0.9_bybase_3mer.txt
